310549671af7fef8507de061c256aa6c7fca12a4
halilibrahimcelik <you@example.com>
2022-06-24 15:32:36 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
       <h1>My react App</h1>
       <h2>Author:Halil İbrahim</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil iste
+        nulla molestiae, ad optio quod unde repudiandae assumenda quia esse.
+        Provident, rem? Ex perspiciatis quae quisquam explicabo molestias
+        tempora officiis.
+      </p>
     </div>
   );
 }
