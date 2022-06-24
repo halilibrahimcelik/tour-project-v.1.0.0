8d8a3b5eb289894f200b8d83f58cdc935c21598d
halilibrahimcelik <halil.celik139@gmail.com>
2022-06-24 16:41:19 +0300

    fixing a bug

--- src/App.js
+++ src/App.js
@@ -3,20 +3,6 @@ function App() {
     <div className="App">
       <h1>My react App</h1>
       <h2>Author:Halil İbrahim</h2>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil iste
-        nulla molestiae, ad optio quod unde repudiandae assumenda quia esse.
-        Provident, rem? Ex perspiciatis quae quisquam explicabo molestias
-        tempora officiis.
-      </p>
-      <form
-        action="
-  "
-      >
-        <input type="text" name="" id="" />
-        <button>click me</button>
-      </form>
-      <h2>Hi there </h2>
     </div>
   );
 }
